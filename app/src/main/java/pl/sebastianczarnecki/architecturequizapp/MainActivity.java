fe859336dedccc0dcbd29778dc7345d2fe3f3a73
Sebastian Czarnecki <32302475+bluewarsaw@users.noreply.github.com>
2018-03-02 17:22:48 +0100

    #cleanercode

--- app/src/main/java/pl/sebastianczarnecki/architecturequizapp/MainActivity.java
+++ app/src/main/java/pl/sebastianczarnecki/architecturequizapp/MainActivity.java
@@ -19,13 +19,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
     // sumUp method counts final score
-
     public void sumUpTwo(View view) {
 
         // check RadioButtons
-
         RadioButton isWarsaw = (RadioButton) findViewById(R.id.warsaw);
         boolean warsaw = isWarsaw.isChecked();
 
@@ -36,15 +33,16 @@ public class MainActivity extends AppCompatActivity {
         boolean warsawThree = isWarsawThree.isChecked();
 
         // check CheckBoxes
-
-        CheckBox modernismA = (CheckBox) findViewById(R.id.modernismanswer);
+        CheckBox modernismA = (CheckBox) findViewById(R.id.cb_modernismanswer_pl);
         boolean ismodernism = modernismA.isChecked();
 
-        CheckBox postwarmodernismB = (CheckBox) findViewById(R.id.postwarmodernismanswer);
+        CheckBox postwarmodernismB = (CheckBox) findViewById(R.id.cb_modernismanswer_fr);
         boolean ispostwarmodernism = postwarmodernismB.isChecked();
 
-        // this part creates String that was given by a player in EditText field and it turns it to lower case
+        CheckBox powstwarmodernismC = (CheckBox) findViewById(R.id.cb_modernismanswer_usa);
+        boolean ispowstwarmodernismC = powstwarmodernismC.isChecked();
 
+        // this part creates String that was given by a player in EditText field and it turns it to lower case
         EditText openText = (EditText) findViewById(R.id.answer);
         String openAnswer = openText.getText().toString().toLowerCase();
 
@@ -52,51 +50,53 @@ public class MainActivity extends AppCompatActivity {
         String openNameAnswer = openName.getText().toString();
 
         //Using "if" statement, method is counting points. It adds 1 point every time the right answer is given
-
-
         if (warsaw) {
-            score = score + 1;
+            score += 1;
         }
 
         if (warsawThree) {
-            score = score + 1;
+            score += 1;
         }
 
         if (warsawTwo) {
-            score = score + 1;
+            score += 1;
         }
-
 
         if (ismodernism && ispostwarmodernism) {
-            score = score + 1;
+            score += 1;
+        } else if (ispowstwarmodernismC){
+            score += 0;
+        } else if (ismodernism && ispostwarmodernism && ispowstwarmodernismC){
+            score += 0;
+        } else {
+            score += 0;
         }
 
-        // answer given in text entry need to contain word "void". Thanks to toLowerCase() method, player receives a point even if he/she types upper case letters.
-
+        // answer given in text entry need to contain word "le corbusier". Thanks to toLowerCase() method, player receives a point even if he/she types upper case letters.
         if (openAnswer.contains("le corbusier")) {
             score = score + 1;
         }
 
         // this creates toast message that shows score of a player
+        if (score > 4){
+            Toast.makeText(this, "Your score: " + score + "/5" + "\n Nice, " + openNameAnswer +"!", Toast.LENGTH_LONG).show();
+        }
 
         if (score > 3){
-            Toast.makeText(this, "Your score: " + score + "/4" + "\n Nice, " + openNameAnswer +"!", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Your score: " + score + "/5" + "\n Nice, " + openNameAnswer +"!", Toast.LENGTH_LONG).show();
         }
 
         if (score < 2){
-            Toast.makeText(this, "Your score: " + score + "/4" + "\n Try better, " + openNameAnswer, Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Your score: " + score + "/5" + "\n Try better, " + openNameAnswer, Toast.LENGTH_LONG).show();
         }
 
         if (score == 2){
-            Toast.makeText(this, "Your score: " + score + "/4" + "\n Try better, " + openNameAnswer, Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Your score: " + score + "/5" + "\n Try better, " + openNameAnswer, Toast.LENGTH_LONG).show();
         }
-
 
         if (score == 3){
-            Toast.makeText(this, "Your score: " + score + "/4" + "\n pretty nice, " + openNameAnswer, Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Your score: " + score + "/5" + "\n pretty nice, " + openNameAnswer, Toast.LENGTH_LONG).show();
         }
         // after toast message, method resets "score" value back to 0.
-
         score = 0;
-
     }}
